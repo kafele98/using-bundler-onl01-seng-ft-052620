c4fa3910aeddc222f1031f43673c8bf1a37f3730
Zoe Keller <keller.zoe98@gmail.com>
2020-06-17 17:11:41 +0000

    Automatically backed up by Learn

--- bin/run.rb
+++ bin/run.rb
@@ -1,10 +1,8 @@
-require_relative '../config/environment'
+require_relative '../config/environment.rb'
 
 Hashie
 Sinatra
 Octokit
 Pry
 AwesomePrint
-
-#this file will start our application. Will require environment file to provide our app with access to our gems 
 
